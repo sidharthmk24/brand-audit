5a786171ee3639a50cd7f9e47f4ee95330bfde24
Sidharth mk <sidharthmk24@gmail.com>
2026-06-02 09:45:01 +0530

    gen pdf done

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   // Future-proof: allow Puppeteer/Chromium to be loaded as external packages
   // in serverless/Node.js runtime (needed for screenshot + PDF pipeline)
   serverExternalPackages: ['puppeteer-core', '@sparticuz/chromium'],
+  outputFileTracingIncludes: {
+    '/api/**/*': ['./node_modules/@sparticuz/chromium/bin/**/*']
+  }
 };
 
 export default nextConfig;
